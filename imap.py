b23f8d38903fa769f3a39ed4e9d857164dead6e4
Amol Mandhane <manamol296@gmail.com>
2012-03-24 05:23:08 -0400

    Fetching code completed

--- imap.py
+++ imap.py
@@ -55,7 +55,55 @@ if not c.row_count:
                         mail['course'] = ''
                         mail['user_id'] = user_id
                         mail['read'] = True
-                        mail['has_attachment'] = False
+
+                        if(email_message['Content-Type'].find("multipart") or email_message['Content-Type'].find("multipart") or email_message['Content-Type'].find("multipart")):
+                                mail['has_attachment'] = False
+                        else:
+                                mail['has_attachment'] = True
+
+                        #Inserting into mails
+                        mail_insert_query = 'INSERT INTO mails VALUES(%s)' % ','.join(['?'] * len(mail))
+                        c.execute(mail_insert_query, mail)
+                        con.commit()
 
 else:
         last_mail_id_query = 'SELECT * FROM `mails` ORDER BY id DESC LIMIT 1'
+        c.execute(last_mail_id_query)
+
+        for row in c:
+                last_id = int(row[1])
+
+        result, raw_list = mailbox.uid("search", None, "ALL")
+        id_list = raw_list[0].split()
+        for index in id_list:
+                if int(index) < int(last_id):
+                        continue
+
+                result, tmp = mailbox.uid('fetch', index, "(RFC822)")
+                raw_email = tmp[0][1]
+                email_message = email.message_from_string(raw_email)
+                del(raw_email)
+                del(tmp)
+                mail = []
+                mail['to'] = email_message['To']
+                mail['from'] = email_message['From']
+                mail['received'] = email_message['Date']
+                mail['subject'] = email_message['Subject']
+                mail['cc'] = email_message['Cc']
+                mail['bcc'] = email_message['Bcc']
+                mail['message'] = get_first_text_block(email_message)
+                mail['uid'] = index
+                mail['course'] = ''
+                mail['user_id'] = user_id
+                mail['read'] = True
+
+                if(email_message['Content-Type'].find("multipart") or email_message['Content-Type'].find("multipart") or email_message['Content-Type'].find("multipart")):
+                        mail['has_attachment'] = False
+                else:
+                        mail['has_attachment'] = True
+
+                #Inserting into mails
+                mail_insert_query = 'INSERT INTO mails VALUES(%s)' % ','.join(['?'] * len(mail))
+                c.execute(mail_insert_query, mail)
+                con.commit()
+
